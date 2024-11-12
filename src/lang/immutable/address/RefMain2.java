0efaa315b533ce4db8b7cb66530d156c654e3fbc
SsangSoo <kss10080@naver.com>
2024-11-13 00:01:38 +0900

    immutable Object

--- src/lang/immutable/address/RefMain2.java
+++ src/lang/immutable/address/RefMain2.java
@@ -1,18 +1,15 @@
 package lang.immutable.address;
 
+public class RefMain2 {
 
-public class PrimitiveMain {
     public static void main(String[] args) {
-        //기본형은 절대로 같은 값을 공유하지 않는다.
-        int a = 10;
-        int b = a; // a -> b, 값 복사후 대입
+        ImmutableAddress a = new ImmutableAddress("서울");
+        ImmutableAddress b = a; // 참조값 대입을 막을 수 방법이 없다.
         System.out.println("a = " + a);
         System.out.println("b = " + b);
 
-        b = 20;
-        System.out.println("20 -> b");
+        System.out.println("부산 -> b");
         System.out.println("a = " + a);
         System.out.println("b = " + b);
-
     }
 }
